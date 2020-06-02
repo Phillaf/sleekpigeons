dd4bad50a74da93d52f15f5d2cdfc45b0c908121
Philippe Lafrance <phil.laf@gmail.com>
2020-06-02 00:24:59 -0400

    Draft a gauge library

---
+++ www/js/sleek-gauge.js
@@ -0,0 +1,101 @@
+class SleekGauge extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.appendChild(gauge.content.cloneNode(true));
+    shadow.appendChild(style.content.cloneNode(true));
+  };
+}
+
+const gauge = document.createElement('template');
+gauge.innerHTML = `
+  <div class="gauge">
+    <p>Top</p>
+    <div class="mask">
+      <div class="semi-circle"></div>
+      <div class="semi-circle-mask"></div>
+      <div class="arrow"></div>
+    </div>
+    <p>Bottom</p>
+  </div>`;
+
+const style = document.createElement('template');
+style.innerHTML = `
+  <style>
+    :host {
+      --outer-size: 12em;
+      --inner-size: 11em;
+      --arrow-thickness: 0.3em;
+      --half-gauge-thickness: calc(calc(var(--outer-size) - var(--inner-size))/4);
+      --rotation: 74deg;
+      --rotation-arrow: 149deg;
+    }
+    .gauge{
+      display: flex;
+      flex-direction: column;
+      width: 12em;
+    }
+    p {
+      text-align: center;
+    }
+    .mask {
+      position: relative;
+      overflow:hidden;
+    }
+    .semi-circle {
+      position: relative;
+      width: var(--outer-size);
+      height: calc(var(--outer-size) / 2);
+      background: var(--light-color);
+      border-radius: 50% 50% 50% 50%/100% 100% 0% 0%;
+    }
+    .semi-circle::before {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 50%;
+      z-index: 2;
+      width: var(--inner-size);
+      height: calc(var(--inner-size) / 2);
+      margin-left: calc(calc(var(--inner-size) / 2) * -1);
+      background: var(--background-color);
+      border-radius: 50% 50% 50% 50%/100% 100% 0% 0%;
+    }
+    .semi-circle-mask {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: var(--outer-size);
+      height: var(--outer-size);
+      background: transparent;
+      transform: rotate(var(--rotation));
+      transform-origin: center;
+      backface-visibility: hidden;
+    }
+    .semi-circle-mask::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 2;
+      width: var(--outer-size);
+      height: calc(var(--outer-size) / 2);
+      margin: -1px 0 0 -1px;
+      background: var(--shade-light-color);
+      border-radius: 50% 50% 50% 50%/100% 100% 0% 0%;
+    }
+    .arrow {
+      position: absolute;
+      z-index: 3;
+      bottom: 0;
+      left: calc(var(--half-gauge-thickness) - calc(var(--arrow-thickness) / 2));
+      width: calc(calc(var(--inner-size)/2) + var(--half-gauge-thickness));
+      height: var(--arrow-thickness);
+      background: var(--shade-dark-color);
+      transform: rotate(var(--rotation-arrow));
+      transform-origin: center right;
+    }
+  </style>`;
+
+customElements.define('sleek-gauge', SleekGauge);
+export {SleekGauge};
